d217331409a8ec8c0955665f7184a280c5429a11
Gardak <55040620+Gardak@users.noreply.github.com>
2019-10-17 15:59:14 -0400

    added files
    added for exercises and project 02

---
+++ exercise/class/template-p5-project/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+let cat;
+let mouse;
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+createCanvas(1000, 1000);
+cat = new Predator(width / 2, height / 2, 5, color(186, 175, 87), 25);
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
